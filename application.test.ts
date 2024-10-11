2bf9e7352bef165e8423991c1c1e4279b7b59ca9
Max Bechtold <max.bechtold@andrena.de>
2024-10-11 16:46:13 +0200

    remove idiotic test

--- application.test.ts
+++ application.test.ts
@@ -1,10 +1,6 @@
 import { main } from "./main";
 
 describe("A Test", () => {
-  it("fails", () => {
-    expect(false as boolean).toEqual(true as boolean);
-  });
-
   it("succeeds", () => {
     expect(true as boolean).toEqual(true as boolean);
   });
